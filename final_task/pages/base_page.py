7ad4c287b921dc0dc92634139a3a7bfce847823c
YanaGrr <johannag.nn@gmail.com>
2023-04-16 14:54:13 +0300

    Проверка элемента на странице

--- final_task/pages/base_page.py
+++ final_task/pages/base_page.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
     def __init__(self, browser, url): #конструктор — метод, который вызывается, когда мы создаем объект; в качестве параметров мы передаем экземпляр драйвера и url адрес, - внутри конструктора сохраняем эти данные, как аттрибуты нашего класса
@@ -6,3 +7,13 @@ class BasePage():
         self.url = url
     def open(self): #метод, который открывает нужную страницу в браузере, используя метод get()
         self.browser.get(self.url)
+    def __init__(self, browser, url, timeout=10): #команда для неявного ожидания со значением по умолчанию в 10
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+    def is_element_present(self, how, what): #метод, в котором будем перехватывать исключение. В него будем передавать два аргумента: как искать (css, id, xpath и тд) и собственно что искать (строку-селектор)
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
--- final_task/pages/main_page.py
+++ final_task/pages/main_page.py
@@ -2,6 +2,8 @@ from .base_page import BasePage
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage): #класс-предок в Python указывается в скобках
-        def go_to_login_page(self):
+        def go_to_login_page(self): #открыть главную страницу
             login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
             login_link.click()
+        def should_be_login_link(self): #проверить, что есть ссылка, которая ведет на логин
+            assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
